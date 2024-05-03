0d155f4fb57116e3def4b5eab49b89b43bd56edb
Ronak Mehta <rmehta004@gmail.com>
2024-05-03 06:57:20 -0700

    Remove distributed dev files.

--- tests/distributed_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-NUM_TRAINERS=3
-
-OMP_NUM_THREADS=1 python3 -m torch.distributed.launch --standalone --nnodes=1 --nproc-per-node=$NUM_TRAINERS tests/distributed_test.py
-# OMP_NUM_THREADS=1 torchrun --standalone --nnodes=1 --nproc-per-node=$NUM_TRAINERS tests/distributed_test.py
