b639370f9c9e90e0c8852cdc7c0925b6421080b5
Ronak Mehta <rmehta004@gmail.com>
2025-04-22 13:36:29 -0700

    Group DRO.

--- deshift/_src/spectral_risk.py
+++ deshift/_src/spectral_risk.py
@@ -18,7 +18,8 @@ def make_spectral_risk_measure(
         which should be the same length as the batch size.
       penalty: either 'chi2' or 'kl' indicating which f-divergence 
         to use as the dual regularizer.
-      shift_cost: ehe non-negative dual regularization parameter.
+      shift_cost: the non-negative dual regularization parameter.
+      group_dist
 
     Returns:
       compute_sample_weight
@@ -30,6 +31,42 @@ def make_spectral_risk_measure(
             device = losses.get_device()
             device = device if device >= 0 else "cpu"
             weights = spectral_risk_measure_maximization_oracle(spectrum, shift_cost, penalty, losses.cpu().numpy())
+        return weights.to(device)
+    return max_oracle
+
+def make_group_spectral_risk_measure(
+        spectrum: np.ndarray,
+        penalty: str="chi2",
+        shift_cost: float=0.0,
+    ):
+    """Create a function which computes the sample weights for Group DRO from a vector of losses when using a spectral risk measure ambiguity set.
+ 
+    Args:
+      spectrum: a Numpy array containing the spectrum weights, 
+        which should be the same length as the number of groups.
+      penalty: either 'chi2' or 'kl' indicating which f-divergence 
+        to use as the dual regularizer.
+
+    Returns:
+      compute_sample_weight
+        a function that maps ``n`` losses to a vector of ``n`` weights on each training example.
+    """
+    def max_oracle(losses, group_labels):
+        assert torch.is_tensor(losses), "`losses` must be a PyTorch tensor"
+        with torch.no_grad():
+            device = losses.get_device()
+            device = device if device >= 0 else "cpu"
+
+            # count average loss of each group
+            unique_labels, labels_count = group_labels.unique(dim=0, return_counts=True)
+            res = torch.zeros_like(unique_labels, dtype=torch.float).scatter_add_(0, group_labels, losses)
+            res = res / labels_count.float().unsqueeze(1)
+            group_weights = spectral_risk_measure_maximization_oracle(spectrum, shift_cost, penalty, res.cpu().numpy())
+
+            # renormalize weight for groups that were observed
+            group_weights[res<=1e-10] = 0.0
+            group_weights /= (group_weights.sum() * labels_count)
+            weights = torch.gather(group_weights, 0, group_labels)
         return weights.to(device)
     return max_oracle
 
